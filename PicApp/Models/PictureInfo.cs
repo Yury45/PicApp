940555c28a9874e19d730ec7d84fecc78f6cf975
Red Jluc <zm.ptz.ru2010@yandex.ru>
2023-09-27 14:21:58 +0500

    Добавьте файлы проекта.

---
+++ PicApp/Models/PictureInfo.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace PicApp.Models
+{
+    public class PictureInfo : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private string _fileName;
+        private string _picturePath;
+        private DateTime _creationDate;
+
+        public string FileName
+        {
+            get { return _fileName; }
+            set
+            {
+                if (_fileName != value)
+                {
+                    _fileName = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        public string PicturePath
+        {
+            get { return _picturePath; }
+            set
+            {
+                if (_picturePath != value)
+                {
+                    _picturePath = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        public DateTime CreationDate
+        {
+            get { return _creationDate; }
+            set
+            {
+                if (_creationDate != value)
+                {
+                    _creationDate = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        public PictureInfo(string fileName, string picturePath, DateTime _creationDate)
+        {
+            _fileName = fileName;
+            _picturePath = picturePath;
+            this._creationDate = _creationDate;
+        }
+    }
+}
---
+++ PicApp/Pages/LoginPage.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PicApp.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginPage : ContentPage
+    {
+        string _password;
+        string _tempPass = String.Empty;
+
+        public LoginPage()
+        {
+            _password = Preferences.Get("Password", String.Empty);
+
+            InitializeComponent();
+        }
+
+        private void EntryPassword_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (_password == String.Empty && _tempPass == String.Empty)
+            {
+                prompt.Text = $"Введите {passwordField.MaxLength-passwordField.Text.Length} символа для PIN-кода";
+                if (passwordField.Text.Length == 4)
+                {
+                    _tempPass = passwordField.Text;
+                    passwordField.Text = String.Empty;
+                    prompt.Text = "";
+                }
+            }
+            else if(_password == String.Empty && _tempPass != String.Empty)
+            {
+                prompt.Text = "Введите PIN-код повторно";
+
+                if (passwordField.Text.Length == 4 && passwordField.Text == _tempPass)
+                {
+                    Preferences.Set("Password", passwordField.Text);
+                    _password = passwordField.Text;
+                    passwordField.Text = String.Empty;
+
+                    Navigation.PushAsync(new GalleryPage());
+                }
+            }
+            else
+            {
+                prompt.Text ="Для доступа к галерее введите пин-код!";
+
+                if(passwordField.Text == _password)
+                {
+                    passwordField.Text = String.Empty;
+                    Navigation.PushAsync(new GalleryPage());
+
+                }
+
+            }
+        }
+    }
+}
